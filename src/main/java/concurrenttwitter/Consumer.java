8bc2af366aae4fd01b4385e5f9feebc010e20f49
Rodrigo <rodrigopds73@gmail.com>
2018-06-11 02:37:58 -0300

    cache

--- src/main/java/concurrenttwitter/Consumer.java
+++ src/main/java/concurrenttwitter/Consumer.java
@@ -8,60 +8,78 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import twitter4j.Status;
 
 public class Consumer implements Runnable {
-    
+
     private BlockingQueue<Status> queue;
-    
-    public Consumer(BlockingQueue<Status> queue) {
+    ConcurrentHashMap<String, String> cache;
+
+    public Consumer(BlockingQueue<Status> queue, ConcurrentHashMap<String, String> cache) {
         this.queue = queue;
+        this.cache = cache;
     }
 
     @Override
     public void run() {
-        while(true) {
+        while (true) {
+
             try {
-                List<String> urls = getURLs(queue.take().getText());
+                Status tweet = queue.take();
+                List<String> urls = getURLs(tweet.getText());
+                
+                System.out.println("ID: " + tweet.getId());
+                System.out.println("User: " + tweet.getUser().getName());
+                System.out.println("Date: " + tweet.getCreatedAt());
+                System.out.println("Text: " + tweet.getText());
+                System.out.println("URLs: ");
                 for (String url : urls) {
+                    String expandedUrl = cache.get(url);
+                    if (expandedUrl == null) {
+                        expandedUrl = expandShortURL(url);
+                        cache.put(url, expandedUrl);
+                    }
                     System.out.println(expandShortURL(url));
                 }
-//                System.out.println(urls);
+                System.out.println("");
             } catch (InterruptedException | IOException ex) {
                 Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-    
+
     private List<String> getURLs(String text) {
         List<String> urls = new ArrayList<>();
-        
+
         for (String token : text.split(" ")) {
 
-            if(token.length() > 7) {
-                
-                if(token.substring(0, 8).equals("https://"))
+            if (token.length() > 7) {
+
+                if (token.substring(0, 8).equals("https://")) {
                     urls.add(token);
-                if(token.substring(0, 7).equals("http://"))
+                }
+                if (token.substring(0, 7).equals("http://")) {
                     urls.add(token);
+                }
             }
         }
         return urls;
     }
-    
+
     private String expandShortURL(String address) throws IOException {
         URL url = new URL(address);
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection(Proxy.NO_PROXY); //using proxy may increase latency
         connection.setInstanceFollowRedirects(false);
         connection.connect();
-        
+
         String expandedURL = connection.getHeaderField("Location");
         connection.getInputStream().close();
-        
+
         return expandedURL;
     }
-    
+
 }
--- src/main/java/concurrenttwitter/TwitterConcurrentProject.java
+++ src/main/java/concurrenttwitter/TwitterConcurrentProject.java
@@ -1,6 +1,7 @@
 package concurrenttwitter;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.LinkedBlockingQueue;
 import twitter4j.*;
@@ -8,24 +9,16 @@ import twitter4j.*;
 public class TwitterConcurrentProject {
     
     public static void main(String[] args) throws TwitterException, InterruptedException, ExecutionException {
-//        BlockingQueue queue = new LinkedBlockingQueue();
-//        QueryProducer queryproducer = new QueryProducer(new Query("security malware"), queue);
-//        queryproducer.setInterval(5000);
-//        Consumer consumer = new Consumer(queue);
-////        ExecutorService executorService = Executors.newFixedThreadPool(15);;
-//        
-//        new Thread(queryproducer).start();
-//        for (int i = 0; i < 15; i++) {
-//            new Thread(consumer).start();
-//        }
 
         BlockingQueue queue = new LinkedBlockingQueue();
+        ConcurrentHashMap<String, String> cache = new ConcurrentHashMap<>();
+        
         String[] keywords = {"infosec","malware"};
         StreamListenerProducer streamlistener = new StreamListenerProducer(keywords);
         streamlistener.addListener(new TwitterStreamListener(queue));
         new Thread(streamlistener).start();
         
-        Consumer consumer = new Consumer(queue);
+        Consumer consumer = new Consumer(queue, cache);
         for (int i = 0; i < 15; i++) {
             new Thread(consumer).start();
         }
